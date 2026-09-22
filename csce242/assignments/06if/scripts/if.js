fa77cc4839005f2186e21029ac18e2f34a46d9f5
breadwards88 <breadwards88@gmail.com>
2026-09-22 03:24:00 -0400

    finished CSS for assignment 8 aswell as more javascript

--- csce242/assignments/06if/scripts/if.js
+++ csce242/assignments/06if/scripts/if.js
@@ -18,8 +18,9 @@ const writeSemester = () => {
     const days = document.getElementById("e2-text");
     const dayComment = document.getElementById("e2-comment")
     const daysLeft = calcSemester();
+    
 
-    days.innerHTML = "You have "+daysLeft+" days left in the semester";
+    days.innerHTML = "You have <strong>" +daysLeft+"</strong> days left in the semester";
     if (daysLeft < 1){
         dayComment.innerHTML = "The semester is over grades are in."
     }
@@ -40,8 +41,9 @@ const writeSemester = () => {
 }
 
 const startSemester = () => {
+    document.getElementById("exercise2").classList.toggle("hidden");
     writeSemester();
-    document.getElementById("exercise1").classList.toggle("hidden");
+
 }
 
 const calcAbsences = () => {
@@ -60,11 +62,26 @@ const writeAbsences = () => {
     const absentDays = document.getElementById("absences").value;
 
     absence.innerHTML = "You will lose "+absencePercentage+"% for skipping "+ absentDays +" days.";
-    absenceComment.innerHTML = "This is not an online class, you are missing valuable learning opportunities";
+    if(absentDays <1) {
+        absenceComment.innerHTML = "Smart of you, every day we learn something new so it's wise not to skip.";    
+    }
+    else if (absentDays < 4) {
+        absenceComment.innerHTML = "This is not encouraged, but atleast you aren't missing out on a large portion.";
+    }
+    else if (absentDays < 10) {
+        absenceComment.innerHTML = "It won't be easy to get an A in this class and this definitly isn't helping.";
+    }
+    else if (absentDays < 15) {
+        absenceComment.innerHTML = "Sure, its not like we don't have holidays why not take half the class off at that point.";
+    }
+    else {
+        absenceComment.innerHTML = "This is not an online class, you are missing valuable learning opportunities";
+    }
+
 }
 
 const startAbcenses = () => {
-    document.getElementById("exercise2").classList.toggle("hidden");
+    document.getElementById("exercise1").classList.toggle("hidden");
 }
 
 const isBlank = (data, id) => {
@@ -76,7 +93,7 @@ const isBlank = (data, id) => {
 }
 
 window.onload = () => {
-    document.getElementById('exercise1-link').onclick=startSemester;
-    document.getElementById('exercise2-link').onclick=startAbcenses;
+    document.getElementById('exercise2-link').onclick=startSemester;
+    document.getElementById('exercise1-link').onclick=startAbcenses;
     document.getElementById('absences').onchange=writeAbsences;
 }
